f8d38d9febe98956ba20d9d2a71202ab51ed3a27
Agikedwin <agikedwin@gmail.com>
2025-07-02 10:19:41 +0300

    Added folder structure

---
+++ README.md
@@ -0,0 +1,9 @@
+##Create virtual environment
+```bash
+python3 -m venv cvenv 
+```
+##Activate virtual environment
+
+```bash
+source cvenv/bin/activate
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Coptic AI Knowledge Base Chatbot',
+    version= '0.0.0',
+    author= 'Agik Edwin',
+    author_email= 'agikedwin@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
