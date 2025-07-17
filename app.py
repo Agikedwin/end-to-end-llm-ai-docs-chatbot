46b354ef7552620fda77e31f9710265ee16f09cb
Agikedwin <agikedwin@gmail.com>
2025-07-17 11:45:20 +0300

    deploy in docker hub

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ docsearch = PineconeVectorStore.from_existing_index(
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":10})
 
 
-llm = OpenAI(temperature=0.8, max_tokens=500)
+llm = OpenAI(temperature=0.6, max_tokens=500)
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
